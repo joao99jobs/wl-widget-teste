b92e6140c7e1831d72f47c1cffc0fd127ecd1a27
João <joao@MacBook-Pro.local>
2023-09-13 13:25:17 -0300

    testes e mais testes

---
+++ README.md
@@ -0,0 +1,14 @@
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+
+
+
+https://stackoverflow.com/questions/72440923/bundle-react-js-project-into-single-js-file-that-can-be-embedded-in-ghost-blog-p
---
+++ vite.config.js
@@ -0,0 +1,26 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), cssInjectedByJsPlugin()],
+  build: {
+    cssCodeSplit: false,
+    rollupOptions: {
+      input: {
+        app: "./src/main.jsx",
+      },
+    },
+  },
+  base: "https://cdn.jsdelivr.net/gh/tony19-sandbox/vite-react-single-js-file/dist/",
+})
